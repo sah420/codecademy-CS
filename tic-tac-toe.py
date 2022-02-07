cbdabdcb3cb0bf42b6e51a2cd998dbd54694a87d
sah420 <99121714+sah420@users.noreply.github.com>
2022-02-07 11:43:41 +0000

    set up board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,1 +1,15 @@
+board = ['-', '-', '-',
+        '-', '-', '-',
+        '-', '-', '-']
+currentPlayer = "X"
+winner = None
+gameRunning = True
 
+def printBoard(board):
+    print(board[0] + ' | ' + board[1] + ' | ' + board[2])
+    print('--------')
+    print(board[3] + ' | ' + board[4] + ' | ' + board[5])
+    print('--------')
+    print(board[6] + ' | ' + board[7] + ' | ' + board[8])
+
+printBoard(board)
